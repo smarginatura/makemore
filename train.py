dedbef2a9c375dc59d8ce76e8a591c814ee65ad0
MZ <mszheng.ai@gmail.com>
2024-06-04 14:00:54 -0700

    restructured

--- train.py
+++ train.py
@@ -10,51 +10,34 @@ from torch.nn import functional as F
 
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
-#from torch.utils.tensorboard import SummaryWriter
 
 from model import ModelConfig, RNN, loss_fn
 from data_loader import CharDataset, create_datasets, InfiniteDataLoader
 
-
 # -----------------------------------------------------------------------------
-# helper functions for evaluating and sampling from the model
-@torch.no_grad()
-def generate(model, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None):
-    """
-    Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
-    the sequence max_new_tokens times, feeding the predictions back into the model each time.
-    Most likely you'll want to make sure to be in model.eval() mode of operation for this.
-    """
-    block_size = model.get_block_size()
-    for _ in range(max_new_tokens):
-        # if the sequence context is growing too long we must crop it at block_size
-        idx_cond = idx if idx.size(1) <= block_size else idx[:, -block_size:]
-        # forward the model to get the logits for the index in the sequence
-        logits = model(idx_cond)
-        # pluck the logits at the final step and scale by desired temperature
-        logits = logits[:, -1, :] / temperature
-        # optionally crop the logits to only the top k options
-        if top_k is not None:
-            v, _ = torch.topk(logits, top_k)
-            logits[logits < v[:, [-1]]] = -float('Inf')
-        # apply softmax to convert logits to (normalized) probabilities
-        probs = F.softmax(logits, dim=-1)
-        # either sample from the distribution or take the most likely element
-        if do_sample:
-            idx_next = torch.multinomial(probs, num_samples=1)
-        else:
-            _, idx_next = torch.topk(probs, k=1, dim=-1)
-        # append sampled index to the running sequence and continue
-        idx = torch.cat((idx, idx_next), dim=1)
+@torch.inference_mode()
+def evaluate(model, dataset, batch_size=50, max_batches=None):
+    model.eval()
+    loader = DataLoader(dataset, shuffle=True, batch_size=batch_size, num_workers=0)
+    losses = []
+    for i, batch in enumerate(loader):
+        batch = [t.to(args.device) for t in batch]
+        X, Y = batch
+        logits = model(X)
+        loss = loss_fn(logits, Y)
+        losses.append(loss.item())
+        if max_batches is not None and i >= max_batches:
+            break
+    mean_loss = torch.tensor(losses).mean().item()
+    model.train() # reset model back to training mode
+    return mean_loss
 
-    return idx
-
-def print_samples(num=10):
+def print_samples(model, num=10):
     """ samples from the model and pretty prints the decoded samples """
     X_init = torch.zeros(num, 1, dtype=torch.long).to(args.device)
     top_k = args.top_k if args.top_k != -1 else None
     steps = train_dataset.get_output_length() - 1 # -1 because we already start with <START> token (index 0)
-    X_samp = generate(model, X_init, steps, top_k=top_k, do_sample=True).to('cpu')
+    X_samp = model.generate(X_init, steps, top_k=top_k, do_sample=True).to('cpu')
     train_samples, test_samples, new_samples = [], [], []
     for i in range(X_samp.size(0)):
         # get the i'th row of sampled integers, as python list
@@ -76,24 +59,6 @@ def print_samples(num=10):
         for word in lst:
             print(word)
     print('-'*80)
-
-@torch.inference_mode()
-def evaluate(model, dataset, batch_size=50, max_batches=None):
-    model.eval()
-    loader = DataLoader(dataset, shuffle=True, batch_size=batch_size, num_workers=0)
-    losses = []
-    for i, batch in enumerate(loader):
-        batch = [t.to(args.device) for t in batch]
-        X, Y = batch
-        logits = model(X)
-        loss = loss_fn(logits, Y)
-        losses.append(loss.item())
-        if max_batches is not None and i >= max_batches:
-            break
-    mean_loss = torch.tensor(losses).mean().item()
-    model.train() # reset model back to training mode
-    return mean_loss
-
 
 if __name__ == '__main__':
     
@@ -140,12 +105,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(vars(args))
 
-
     # system inits
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
     os.makedirs(args.work_dir, exist_ok=True)
-    #writer = SummaryWriter(log_dir=args.work_dir)
 
     # init datasets
     train_dataset, test_dataset = create_datasets(args.input_file)
@@ -220,7 +183,7 @@ if __name__ == '__main__':
         if step > 0 and step % 500 == 0:
             train_loss = evaluate(model, train_dataset, batch_size=100, max_batches=10)
             test_loss  = evaluate(model, test_dataset,  batch_size=100, max_batches=10)
-
+            # logging
             print(f"step {step} train loss: {train_loss:.4f} test loss: {test_loss:.4f}")
             # save the model to disk if it has improved
             if best_loss is None or test_loss < best_loss:
@@ -231,7 +194,7 @@ if __name__ == '__main__':
 
         # sample from the model
         if step > 0 and step % 200 == 0:
-            print_samples(num=10)
+            print_samples(model, num=10) 
 
         step += 1
         # termination conditions
